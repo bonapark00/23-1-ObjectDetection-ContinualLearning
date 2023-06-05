7ed8fea64e52978e614a00f3978f6257dc6c9070
seongwon980 <poiuy98749@yonsei.ac.kr>
2023-06-05 14:33:34 +0000

    Add shell script for automated torchvision library replacement

---
+++ scripts/update_torchvision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the directories
+src_dir="./detection"
+dest_dir="/opt/conda/lib/python3.7/site-packages/torchvision/models/detection"
+
+# Delete the existing destination directory
+rm -rf "$dest_dir"
+
+# Copy the source directory to the destination
+cp -R "$src_dir" "$dest_dir"

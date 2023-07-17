7784e7ccda8e56691a73afcd41ff7e7764eb6d06
caisarl76 <caisarl76@gmail.com>
2023-07-17 17:01:42 +0000

    Add: Modified shell script to install detectron2

--- settings/install_requirements.sh
+++ settings/install_requirements.sh
@@ -4,7 +4,7 @@
 apt-get update -y
 
 # Install necessary packages
-apt-get install -y git gcc
+apt-get install -y git gcc g++
 apt-get install unzip
 
 # Install Python packages
@@ -12,6 +12,7 @@ pip install pycocotools matplotlib opencv-contrib-python
 pip install faiss
 pip install faiss-cpu --no-cache
 pip install h5py
+python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
 # Give execute permissions to the update_torchvision.sh script
 chmod +x scripts/update_torchvision.sh

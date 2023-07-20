ce8f3a7bfa94e9e41f8a24d71aea568382479a0a
carpedkm <flytodk@gmail.com>
2023-07-20 06:14:32 +0000

    Add: Included ssl shift downloading script using dropbox

---
+++ dataset/ssl_shift_dropbox.sh
@@ -0,0 +1,3 @@
+mkdir -p ./precomputed_proposals
+wget -O ./precomputed_proposals/ssl_shift.tar https://www.dropbox.com/scl/fi/odykro6p8mn7c3qe1ixaj/ssl_shift.tar?rlkey=gxyj9mkixclz9a7r1b33ktv5s&dl=0
+tar -xvf ./precomputed_proposals/ssl_shift.tar -C ./precomputed_proposals/
